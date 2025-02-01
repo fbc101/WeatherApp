8e671c7385d88fcaa7b3a58a516737aed5b770e4
Franci Branda-CHen <fbc101@seas.upenn.edu>
2025-02-01 16:14:29 -0500

    added clock

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Clock from '../components/clock';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Clock />
     </div>
   );
 }
