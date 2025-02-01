6e11e321a217db12620a27d16cddd505957a3880
Franci Branda-CHen <fbc101@seas.upenn.edu>
2025-02-01 16:25:34 -0500

    replaced clock with librabry

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,11 +1,29 @@
-import './App.css';
-import Clock from '../components/clock';
+import { useEffect, useState } from 'react';
+import Clock from 'react-clock';
+import 'react-clock/dist/Clock.css';
 
 
 function App() {
+  const [value, setValue] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => setValue(new Date()), 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <Clock />
+    <div>
+      <p>Current time:</p>
+      <Clock 
+        value={value} 
+        renderNumbers={true} 
+        renderHourMarks={false}
+        renderMinuteMarks={false}
+        size={150}
+      />
     </div>
   );
 }
