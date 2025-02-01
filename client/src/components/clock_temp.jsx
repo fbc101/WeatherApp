0fec70d00869d24530ce042f5cbf603075163c1a
Franci Branda-CHen <fbc101@seas.upenn.edu>
2025-02-01 17:18:53 -0500

    added clock temperature

--- client/src/components/clock_temp.jsx
+++ client/src/components/clock_temp.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import Clock from 'react-clock';
 import 'react-clock/dist/Clock.css';
-
+import './clock_temp.css'
 
 function ClockTemp() {
   const [value, setValue] = useState(new Date());
@@ -16,14 +16,27 @@ function ClockTemp() {
 
   return (
     <div>
-      <p>Current time:</p>
-      <Clock 
+      <Clock className="react-lib-clock"
         value={value} 
         renderNumbers={true} 
         renderHourMarks={false}
         renderMinuteMarks={false}
         size={150}
       />
+      <div className="surrounding-numbers">
+        <span className="twelve">12</span>
+        <span className="one">1</span>
+        <span className="two">2</span>
+        <span className="three">3</span>
+        <span className="four">4</span>
+        <span className="five">5</span>
+        <span className="six">6</span>
+        <span className="seven">7</span>
+        <span className="eight">8</span>
+        <span className="nine">9</span>
+        <span className="ten">10</span>
+        <span className="eleven">11</span>
+      </div>
     </div>
   );
 }
